3f1de156f5195bfa2e6ee6a2fab362496d17c14a
phuongngan300508-coder <phuongngan300508@gmail.com>
2026-04-10 00:16:40 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,23 @@
-importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging-compat.js');
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/9.x.x/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.x.x/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDV8KseaFVPPcpEU0-jrqqxnGG6xLyJGfE",
-    projectId: "sweetie-game-world",
-    messagingSenderId: "439230414082",
-    appId: "1:439230414082:web:e89f8e86fda4c4dbd2161b"
+  apiKey: "AIzaSyDV8KseaFVPPcpEU0-jrqqxnGG6xLyJGfE",
+  projectId: "sweetie-game-world",
+  messagingSenderId: "439230414082",
+  appId: "1:439230414082:web:e89f8e86fda4c4dbd2161b"
 });
 
 const messaging = firebase.messaging();
 
-// Lắng nghe thông báo khi tắt web
+// Xử lý khi thông báo đến trong khi web đang đóng
 messaging.onBackgroundMessage((payload) => {
-    console.log("Nhận thông báo khi tắt web:", payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: '/logo.png' // Henry thay bằng link ảnh logo game của bạn
-    };
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://i.postimg.cc/J7bjdwFH/Gemini-Generated-Image-(3).png'
+  };
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
